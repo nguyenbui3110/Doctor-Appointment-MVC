368eb95a571adf308ba01095a315a53a152abc7d
nguyenbui3110 <102200221@sv1.dut.udn.vn>
2024-12-02 10:26:42 +0700

    feat: update layout

--- DoctorAppointment.Domain/Enums/Specialization.cs
+++ DoctorAppointment.Domain/Enums/Specialization.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DoctorAppointment.Domain.Enums;
 
 public enum Specialization
 {
-    GeneralPractitioner = 1,
-    Cardiologist = 2,
-    Dermatologist = 3,
-    Neurologist = 4,
-    Pediatrician = 5,
+    [Display(Name = "Đa khoa")]
+    GeneralPractitioner = 1, //đa khoa
+    [Display(Name = "Tim mạch")]
+    Cardiologist = 2, // tim mạch
+    [Display(Name = "Da liễu")]
+    Dermatologist = 3, //da liễu
+    [Display(Name = "Thần kinh")]
+    Neurologist = 4, //thần kinh
+    [Display(Name = "Nhi khoa")]
+    Pediatrician = 5, //khi khoa
 }
